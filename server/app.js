5c0bb551a30b8d4a49f8e4f6ccca91e761127358
Cecilia <Cecilia@example.com>
2024-04-22 22:20:31 -0700

    fix: conflix

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import { readOrders } from "./controllers/read-orders-controller.js";
-import { createOrderController } from "./controllers/create-orders-controller.js";
-import { editOrders } from "./controllers/edit-orders-controller.js";
-import { deleteOrders } from "./controllers/delete-orders-controller.js";
+
+import { router } from "./controllers";
 
 dotenv.config();
 
@@ -11,13 +9,7 @@ const app = express();
 
 // middleware or router
 app.use(express.json()); // 使express支持JSON格式的输入
-
-app.use("/orders", createOrderController);
-app.use("/orders", readOrders);
-app.use("/orders", editOrders);
-app.use("/orders", deleteOrders);
-
-app.get("/orders", readOrders);
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
