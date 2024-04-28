c8f5333a21a2ecc08e0566d15edbf5d50ab41957
Cecilia <Cecilia@example.com>
2024-04-27 17:10:38 -0700

    feat: connect

--- src/pages/playground/playground.jsx
+++ src/pages/playground/playground.jsx
@@ -1,33 +1,81 @@
 import React from "react";
 import "./playground.css";
 import axios from "axios";
-import logo from "/Applications/soure/softeditlearning/accountingapp/src/pages/playground/logo.png";
 
 // fetch, axios
 
 function Playground() {
 	const handleCreateOrder = () => {
-		axios.post("/v1/order/create", { id: 1 });
+		axios.post("/v1/order/create", {
+			id: 1,
+			title: "1",
+			type: "wants",
+			desc: "lot",
+		});
+	};
+	const handleUpdateOrders = () => {
+		axios.post("/v1/order/create", {
+			orderId: 2,
+			newName: "Gas",
+			newAssigned: 300,
+			title: "1",
+			type: "wants",
+			desc: "lot",
+		});
 	};
 	const handleReadOrders = () => {
-		axios.get("/orders", { id: 2 });
+		axios.post("/v1/order/create", {
+			id: 2,
+			title: "1",
+			type: "wants",
+			desc: "lot",
+			limit: 99,
+			offset: 5,
+		});
+	};
+	const handleReadOrder = () => {
+		axios.post("/v1/order/create", {
+			id: 2,
+			title: "1",
+			type: "wants",
+			desc: "lot",
+			name: "water",
+			assigned: 200,
+		});
+	};
+	const handleDeleteOrders = () => {
+		axios.post("/v1/order/create", {
+			id: 2,
+			title: "1",
+			type: "wants",
+			desc: "lot",
+		});
 	};
 
 	return (
 		<div className="homepage">
 			<div className="header">
 				{/* <img src={logo} alt="Logo" className="logo" /> */}
-				<h1>Take Control of Your Finances</h1>
+				{/* <h1>Take Control of Your Finances</h1>
 				<p>Simpliify Your Money with Ease</p>
 				<button className="start-button" type="button">
 					Get Started
-				</button>
-				{/* <button type="button" onClick={handleCreateOrder}>
+				</button> */}
+				<button type="button" onClick={handleCreateOrder}>
 					create order
 				</button>
 				<button type="button" onClick={handleReadOrders}>
 					read orders
-				</button> */}
+				</button>
+				<button type="button" onClick={handleUpdateOrders}>
+					Update Orders
+				</button>
+				<button type="button" onClick={handleReadOrder}>
+					read order
+				</button>
+				<button type="button" onClick={handleDeleteOrders}>
+					Delete Orders
+				</button>
 			</div>
 		</div>
 	);
